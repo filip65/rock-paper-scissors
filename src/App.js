ca0f90b4a5a02af38eda846088c4a14557d2d57c
Filip Katusin <filip.katusin@gmail.com>
2021-08-02 19:07:28 +0200

    upravi scss pre buttony a vytvorenie komponentu pre button

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
+import { useState } from "react";
 import "./App.scss";
 
 import Header from "./components/Header";
-import Buttons from "./components/Buttons";
+import Choosing from "./components/Choosing";
+import Result from "./components/Result";
 
 function App() {
+  const [isChoosing, setIsChoosing] = useState(true);
+  const [userChoise, setUserChoise] = useState("null");
+
   return (
     <div className="App">
       <Header />
-      <Buttons />
+
+      {isChoosing && (
+        <Choosing setUserChoise={setUserChoise} setIsChoosing={setIsChoosing} />
+      )}
+
+      {!isChoosing && <Result />}
     </div>
   );
 }
