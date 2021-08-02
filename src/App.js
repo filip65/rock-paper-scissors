54dc5336b8d41698ff13cd4f1b35bff847ba16ee
Filip Katusin <filip.katusin@gmail.com>
2021-08-02 16:20:24 +0200

    pridanie buttonov

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.scss";
 
 import Header from "./components/Header";
+import Buttons from "./components/Buttons";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Buttons />
     </div>
   );
 }
